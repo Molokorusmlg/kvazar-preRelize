79cd03bec8f33530f5644f999f495f080299f47d
ntr07 <ntr.0707@mail.ru>
2024-10-31 19:27:52 +0300

    create task

--- kwazar/videos/models.py
+++ kwazar/videos/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from users.models import Category_course
 # Create your models here.
 class VideosModel(models.Model):
     name = models.CharField(verbose_name="video",max_length=100)
     description = models.TextField(verbose_name="description",max_length=200)
     file = models.FileField(upload_to="videos_file")
+    course = models.ForeignKey(to = Category_course, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name}"
--- kwazar/videos/views.py
+++ kwazar/videos/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import VideosModel
 from .serializer import VideoSerializer
-from rest_framework.generics import ListAPIView,RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from users.models import Category_course
 
 
 # Create your views here.
@@ -11,21 +12,22 @@ from rest_framework.response import Response
 class ListVideos(APIView):
 
     def get(self, request, format=None):
-        if request.user:
-            if request.user.is_authenticated:
-                videos = VideosModel.objects.all()
-                serializer = VideoSerializer(videos, many=True)
-                print(request.user)
-        
-                return Response(serializer.data)
-            else:
-                return Response({"stop":"нельзя"})
-        else:
+        if request.user.is_authenticated:
+            db_course = Category_course.objects.get(name=request.user.course.name)
+            videos = VideosModel.objects.filter(course=db_course)
+            serializer = VideoSerializer(videos, many=True)
+            for i in range(len(serializer.data)):
+                id_new = serializer.data[i]['course']
+                if id_new is not None:
+                    res = Category_course.objects.filter(id=int(id_new)).first()
+                if res is not None:
+                    serializer.data[i]['course'] = str(res.name)
 
-            return Response({"stop":"нельзя"})
+            return Response(serializer.data)
+        else:
+            return Response({"stop": "нельзя"})
 
 
 class SpecificVideo(RetrieveAPIView):
     queryset = VideosModel.objects.all()
     serializer_class = VideoSerializer
-
